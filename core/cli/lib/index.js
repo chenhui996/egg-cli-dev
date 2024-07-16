ea6d72f2a0f90a8de4f12852da7b147d8eab2dd1
chenhui996 <chenhui996@example.com>
2024-07-16 23:32:03 +0800

    feat: check node version

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -6,15 +6,40 @@ module.exports = core;
 // .js -> module.exports/exports
 // .json -> JSON.parse
 // any -> .js
+
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
 const log = require('@egg-cli-2024/log');
+const constant = require('./const');
 
-function core() {
-    checkPkgVersion();
+// 检查 Node 版本
+function checkNodeVersion() {
+    // 第一步，获取当前 Node 版本
+    const currentVersion = process.version;
+    // 第二步，获取最低 Node 版本
+    const lowestVersion = constant.LOWEST_NODE_VERSION;
+    // 第三步，比对当前版本和最低版本
+    if (!semver.gte(currentVersion, lowestVersion)) {
+        throw new Error(colors.red(`egg-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`));
+    } else {
+        log.info('cli', `Node.js 版本检查通过, 当前版本：${currentVersion}`);
+    }
 }
 
 // 检查版本号
 function checkPkgVersion() {
-    console.log(pkg.version);
     log.info('cli', pkg.version);
 }
+
+// 主函数
+function core() {
+    try {
+        checkPkgVersion();
+        checkNodeVersion();
+    } catch (error) {
+        log.error(error.message);
+    }
+}
+
+
