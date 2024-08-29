a4b606a6768fe9c4be02874674f54141fe87a72e
chenhui996 <chen562450669@qq.com>
2024-08-29 18:42:00 +0800

    feat: exec -> use spawn start process

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -55,14 +55,25 @@ async function exec() {
 
     if (rootFile) {
         try {
-
             // 本行代码解释：require(rootFile)返回的是一个函数，然后调用这个函数
             // rootFile 传入的是一个文件路径，这个文件路径是一个js文件，这个js文件是一个模坩，这个模坩导出了一个函数
             // 所以这行代码的意思是：调用这个模坩导出的函数，并传入arguments
             // require(rootFile).call(null, Array.from(arguments));
 
             // 在 node 子进程中调用
-            const code = 'console.log("hello world")';
+
+            // 拼接 code 可执行文件 -----------------------------------------------
+            const args = Array.from(arguments);
+            const cmd = args[args.length - 1];
+            const o = Object.create(null);
+            Object.keys(cmd).forEach(key => {
+                if (cmd.hasOwnProperty(key) && !key.startsWith('_') && key !== 'parent') {
+                    o[key] = cmd[key];
+                }
+            });
+            args[args.length - 1] = o;
+            const code = `require('${rootFile}').call(null, ${JSON.stringify(args)})`;
+            // -------------------------------------------------------------------
             const child = cp.spawn('node', ['-e', code], {
                 cwd: process.cwd(),
                 stdio: 'inherit' // 作用：子进程的输入输出流和父进程的输入输出流是一样的
