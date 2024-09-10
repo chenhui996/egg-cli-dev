db272c5b5a033f89f5913f35a5a4b6122855db4b
chenhui996 <chen562450669@qq.com>
2024-09-10 23:09:57 +0800

    feat: init -> add is cwd empty

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const Command = require('@egg-cli-2024/command')
 const log = require('@egg-cli-2024/log');
 
@@ -12,7 +13,24 @@ class InitCommand extends Command {
     }
 
     exec() {
-        console.log('exec');
+        try {
+            // 1. 准备阶段
+            this.prepare();
+            // 2. 下载模板
+            // 3. 安装模板
+        } catch (error) {
+            log.error(error.message);
+            if (process.env.LOG_LEVEL === 'verbose') {
+                console.log(error);
+            }
+        }
+    }
+
+    prepare() {
+        // 1. 判断当前目录是否为空
+        // 2. 是否启动强制更新
+        // 3. 选择创建或者取消
+        // 4. 获取项目基本信息 
     }
 }
 
