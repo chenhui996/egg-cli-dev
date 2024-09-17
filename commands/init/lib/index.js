10e53c03daa314e5101bca5bddc1350906407546
chenhui996 <chen562450669@qq.com>
2024-09-17 23:17:12 +0800

    feat: add user process -> name | version | tempalte

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -6,6 +6,9 @@ const fse = require('fs-extra');
 const Command = require('@egg-cli-2024/command')
 const log = require('@egg-cli-2024/log');
 
+const TYPE_PROJECT = 'project';
+const TYPE_COMPONENT = 'component';
+
 class InitCommand extends Command {
     init() {
         this.projectName = this._argv[0] || '';
@@ -34,6 +37,8 @@ class InitCommand extends Command {
     async prepare() {
         // 1. 判断当前目录是否为空
         const localPath = process.cwd(); // 当前命令行执行命令的路径
+
+        // 处理文件夹相关
         if (!this.isDirEmpty(localPath)) {
             let pass = false;
             // 询问是否创建
@@ -46,7 +51,7 @@ class InitCommand extends Command {
                 });
 
                 if (!isContinue) {
-                    return false;
+                    return;
                 }
 
                 pass = isContinue;
@@ -68,10 +73,70 @@ class InitCommand extends Command {
                 }
             }
         }
-        return true;
 
-        // 3. 选择创建或者取消
-        // 4. 获取项目基本信息 
+        return await this.getProjectInfo(); // 获取项目基本信息
+    }
+
+    // 获取项目基本信息
+    async getProjectInfo() {
+        const projectInfo = {};
+        // 1. 选择创建或者取消
+        const { type } = await inquirer.prompt({
+            type: 'list',
+            name: 'type',
+            message: '请选择初始化类型',
+            default: TYPE_PROJECT,
+            choices: [
+                { name: '项目', value: TYPE_PROJECT },
+                { name: '组件', value: TYPE_COMPONENT }
+            ]
+        }); // 选择创建项目或者组件
+
+        log.verbose('type', type);
+        // 获取 项目 基本信息 
+        if (type === TYPE_PROJECT) {
+            const o = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'projectName',
+                    message: '请输入项目名称',
+                    default: '',
+                    validate: function (v) {
+                        return typeof v === 'string' && v.length > 0;
+                    },
+                    filter: function (v) {
+                        return v;
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'projectVersion',
+                    message: '请输入项目版本号',
+                    default: '1.0.0',
+                    validate: function (v) {
+                        return typeof v === 'string' && v.length > 0;
+                    },
+                    filter: function (v) {
+                        return v;
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'projectTemplate',
+                    message: '请选择项目模板',
+                    choices: [
+                        { name: 'Vue', value: 'vue' },
+                        { name: 'React', value: 'react' }
+                    ]
+                }
+            ]);
+
+            log.verbose('o', o);
+        } else if (type === TYPE_COMPONENT) {
+            // 获取 组件 基本信息
+        }
+
+        return projectInfo;
     }
 
     isDirEmpty(localPath) {
